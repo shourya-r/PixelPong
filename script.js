6131095f11236aa39df7c60ddf4c713b25cf96fa
shourya-r <rajputshourya7@gmail.com>
2024-07-01 21:44:20 +0530

    Added functions for detecting when the ball is out and updating score

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ import { Paddle } from "./paddle.js";
 const ball = new Ball(document.querySelector("#ball"));
 const playerPaddle = new Paddle(document.querySelector("#player-paddle"));
 const computerPaddle = new Paddle(document.querySelector("#computer-paddle"));
+const computerScore = document.querySelector(".computer-score");
+const playerScore = document.querySelector(".player-score");
+
 let lastTime;
 function update(time) {
   // if lastTime is not NULL then we update
@@ -13,11 +16,34 @@ function update(time) {
     // we pass delta because it fluctuates
     ball.update(delta);
     computerPaddle.update(delta, ball.y);
+
+    if (isBallOut()) {
+      handleLose();
+    }
   }
 
   lastTime = time;
   window.requestAnimationFrame(update);
 }
+
+function isBallOut() {
+  const rect = ball.rect();
+  return rect.left <= 0 || rect.right >= window.innerWidth;
+}
+
+function handleLose() {
+  const rect = ball.rect();
+  if (rect.right >= window.innerWidth) {
+    const currScore = Number(playerScore.textContent);
+    playerScore.textContent = currScore + 1;
+  } else {
+    const currScore = Number(computerScore.textContent);
+    computerScore.textContent = currScore + 1;
+  }
+  ball.reset();
+  computerPaddle.reset();
+}
+
 // player paddle will move wherever the mouse moves
 document.addEventListener("mousemove", (e) => {
   playerPaddle.position = (e.y / window.innerHeight) * 100;
