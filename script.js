8ddadf5a870dae43bccb2734b2878232cbe4debd
shourya-r <rajputshourya7@gmail.com>
2024-07-01 16:55:50 +0530

    Wrote initial update loop code

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 import { Ball } from "./ball.js";
 
 const ball = new Ball(document.querySelector("#ball"));
+let lastTime;
+function update(time) {
+  // if lastTime is not NULL then we update
+  if (lastTime != null) {
+    // find time which has passed since next frame
+    const delta = time - lastTime;
+    // we pass delta because it fluctuates
+    ball.update(delta);
+  }
 
-console.log(ball);
+  lastTime = time;
+  window.requestAnimationFrame(update);
+}
+
+window.requestAnimationFrame(update);
