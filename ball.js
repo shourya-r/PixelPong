425f7af88558fee64e736d53dfc63e00c76620f8
shourya-r <rajputshourya7@gmail.com>
2024-07-01 17:11:52 +0530

    Added getter and setter for y position

--- ball.js
+++ ball.js
@@ -2,15 +2,26 @@ class Ball {
   constructor(ballElem) {
     this.ballElem = ballElem;
   }
+
   // update x and y position with getter and setter
   get x() {
     return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
   }
+
   set x(value) {
     this.ballElem.style.setProperty("--x", value);
   }
+  get y() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"));
+  }
+
+  set y(value) {
+    this.ballElem.style.setProperty("--y", value);
+  }
+
   update(delta) {
-    this.x = 5;
+    this.x = 60;
+    this.y = 59;
   }
 }
 
