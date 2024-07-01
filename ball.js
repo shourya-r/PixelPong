ba9e68069cc01782fdf217973f4024850536ece8
shourya-r <rajputshourya7@gmail.com>
2024-07-01 17:08:47 +0530

    Turned the factory into a class for using getter and setter
    Added getter and setter for x position of the ball

--- ball.js
+++ ball.js
@@ -1,5 +1,17 @@
-function Ball(ballElem) {
-  return { ballElem };
+class Ball {
+  constructor(ballElem) {
+    this.ballElem = ballElem;
+  }
+  // update x and y position with getter and setter
+  get x() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
+  }
+  set x(value) {
+    this.ballElem.style.setProperty("--x", value);
+  }
+  update(delta) {
+    this.x = 5;
+  }
 }
 
 export { Ball };
